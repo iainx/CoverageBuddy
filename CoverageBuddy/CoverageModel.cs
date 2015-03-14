798153fce9a48e21b678b5688b4759d47fe9bef0
iain holmes <iain@xamarin.com>
2015-03-14 21:46:34 +0000

    An initial commit

---
+++ CoverageBuddy/CoverageModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CoverageBuddy
+{
+	public class CoverageModel
+	{
+		public class CoverageAssembly {
+			public string Name;
+			public string Guid;
+			public string Filename;
+			public int NumberOfMethods;
+			public int FullyCovered;
+			public int PartiallyCovered;
+
+			public Dictionary<string, CoverageClass> Classes;
+		};
+
+		public class CoverageFile {
+			public string Filename;
+			public Dictionary<int, CoverageStatement> Statements;
+		};
+
+		public class CoverageClass {
+			public CoverageAssembly Assembly;
+			public string Name;
+			public List<CoverageMethod> ClassMethods;
+			public Dictionary<string, CoverageFile> ClassFiles;
+		};
+			
+		public class CoverageStatement {
+			public int Offset;
+			public int Counter;
+			public string Filename;
+			public int Line;
+			public int Column;
+		};
+
+		public class CoverageMethod {
+			public CoverageAssembly Assembly;
+			public string ClassName;
+			public string MethodName;
+			public Dictionary<int, CoverageStatement> Statements;
+		};
+
+		public Dictionary<string, CoverageAssembly> Assemblies { get; private set; }
+		public List<CoverageMethod> Methods { get; private set; }
+		public Dictionary<string, CoverageClass> Classes { get; private set; }
+
+		public CoverageModel (string filename)
+		{
+			XDocument document = XDocument.Load (filename);
+
+			Classes = new Dictionary<string, CoverageClass> ();
+
+			var a = from assembly in document.Descendants ("assembly")
+			        select new CoverageAssembly {
+				Name = (string)assembly.Attribute ("name"),
+				Guid = (string)assembly.Attribute ("guid"),
+				Filename = (string)assembly.Attribute ("filename"),
+				NumberOfMethods = (int)assembly.Attribute ("method-count"),
+				FullyCovered = (int)assembly.Attribute ("full"),
+				PartiallyCovered = (int)assembly.Attribute ("partial"),
+				Classes = new Dictionary<string, CoverageClass> ()
+			};
+
+			Assemblies = new Dictionary<string, CoverageAssembly> ();
+			foreach (var assembly in a) {
+				Assemblies[assembly.Name] = assembly;
+			}
+
+			Methods = new List<CoverageMethod> ();
+
+			var ms = document.Descendants ("method");
+			foreach (var m in ms) {
+				CoverageAssembly assembly = Assemblies [(string)m.Attribute ("assembly")];
+
+				CoverageMethod method = new CoverageMethod {
+					Assembly = assembly,
+					ClassName = (string)m.Attribute ("class"),
+					MethodName = (string)m.Attribute ("name"),
+					Statements = new Dictionary<int, CoverageStatement> ()
+				};
+
+				CoverageClass klass;
+				if (!Classes.ContainsKey (method.ClassName)) {
+					klass = new CoverageClass {
+						Assembly = assembly,
+						Name = method.ClassName,
+						ClassMethods = new List<CoverageMethod> (),
+						ClassFiles = new Dictionary<string, CoverageFile> ()
+					};
+
+					Classes [method.ClassName] = klass;
+				} else {
+					klass = Classes [method.ClassName];
+				}
+				assembly.Classes [method.ClassName] = klass;
+
+				klass.ClassMethods.Add (method);
+
+				var s = from statement in m.Descendants ("statement")
+					select new CoverageStatement {
+						Offset = (int)statement.Attribute("offset"),
+						Counter = (int)statement.Attribute("counter"),
+						Filename = (string)statement.Attribute("filename"),
+						Line = (int)statement.Attribute("line"),
+						Column = (int)statement.Attribute("column")
+				};
+
+				foreach (var statement in s) {
+					method.Statements [statement.Line] = statement;
+
+					CoverageFile file;
+
+					if (klass.ClassFiles.ContainsKey (statement.Filename)) {
+						file = klass.ClassFiles [statement.Filename];
+					} else {
+						file = new CoverageFile {
+							Filename = statement.Filename,
+							Statements = new Dictionary<int, CoverageStatement> ()
+						};
+						klass.ClassFiles [statement.Filename] = file;
+					}
+
+					file.Statements[statement.Line] = statement;
+				}
+
+				Methods.Add (method);
+			}
+		}
+	}
+}
+
---
+++ CoverageBuddy/MainWindow.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using Gtk;
+using Glade;
+
+using CoverageBuddy;
+using IgeMacIntegration;
+
+public partial class MainWindow: Gtk.Window
+{
+	private TreeStore coverageAsTreeModel;
+	private CoverageModel model;
+	public CoverageModel Model { 
+		get { return model; }
+		set { 
+			model = value;
+
+			coverageAsTreeModel = new TreeStore (typeof (string), typeof (string), typeof (string), typeof (string));
+			foreach (var assemblyPair in model.Assemblies) {
+				CoverageModel.CoverageAssembly assembly = assemblyPair.Value;
+
+				int percentage = ((assembly.FullyCovered + assembly.PartiallyCovered) * 100) / assembly.NumberOfMethods;
+				TreeIter iter = coverageAsTreeModel.AppendValues (String.Format ("{0} {1}% covered", assembly.Name, percentage), assembly, null, null);
+
+				if (model.Methods == null) {
+					continue;
+				}
+					
+				foreach (var classPair in assembly.Classes) {
+					CoverageModel.CoverageClass klass = classPair.Value;
+					TreeIter classIter = coverageAsTreeModel.AppendValues (iter, klass.Name, null, klass.Name, null);
+
+					/*
+					var classMethods = model.Methods.Where (m => m.ClassName == method.ClassName);
+					foreach (CoverageModel.CoverageMethod classMethod in classMethods) {
+						coverageAsTreeModel.AppendValues (classIter, classMethod.MethodName, null, null, classMethod.MethodName);
+					}
+					*/
+				}
+			}
+
+			coverageTreeView.Model = coverageAsTreeModel;
+		}
+	}
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		Title = "CoverageBuddy";
+
+		SetupTreeView ();
+
+		SetupMacIntegration ();
+
+	}
+
+	void SetupMacIntegration ()
+	{
+		IgeMacMenu.GlobalKeyHandlerEnabled = true;
+		IgeMacMenu.MenuBar = mainMenuBar;
+
+		mainMenuBar.Hide ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	enum CoverageType {
+		NotProfiled,
+		Covered,
+		NotCovered
+	};
+
+	void AddLineToBuffer (string line, int lineNumber, CoverageType type) {
+		TextBuffer buffer = fileTextView.Buffer;
+		TextIter iter = buffer.GetIterAtLine (lineNumber);
+
+		switch (type) {
+		case CoverageType.NotProfiled:
+			buffer.Insert (ref iter, line + '\n');
+			break;
+
+		case CoverageType.Covered:
+			buffer.InsertWithTagsByName (ref iter, line + '\n', "green");
+			break;
+
+		case CoverageType.NotCovered:
+			buffer.InsertWithTagsByName (ref iter, line + '\n', "red");
+			break;
+		}
+	}
+
+	private void SetupTreeView () {
+		TextBuffer buffer = fileTextView.Buffer;
+		TextTag redTag = new TextTag ("red");
+		TextTag greenTag = new TextTag ("green");
+
+		redTag.Foreground = "red";
+		buffer.TagTable.Add (redTag);
+
+		greenTag.Foreground = "green";
+		buffer.TagTable.Add (greenTag);
+
+		TreeViewColumn assemblyColumn = new TreeViewColumn ();
+		assemblyColumn.Title = "Assemblies";
+
+		coverageTreeView.AppendColumn ("Assemblies", new CellRendererText (), "text", 0);
+
+		TreeSelection selection = coverageTreeView.Selection;
+		selection.Changed += (o, e) => {
+			TreeIter iter;
+
+			if (selection.GetSelected(out iter)) {
+				string className = coverageAsTreeModel.GetValue (iter, 2) as string;
+				if (className == null) {
+					return;
+				}
+
+				CoverageModel.CoverageClass klass = model.Classes[className];
+
+				var pair = klass.ClassFiles.FirstOrDefault();
+				CoverageModel.CoverageFile file = pair.Value;
+				string filename = file.Filename;
+				if (filename == null) {
+					return;
+				}
+
+				titleLabel.Text = filename;
+
+				fileTextView.Buffer.Text = "";
+				string[] fileContents = System.IO.File.ReadAllLines (filename);
+
+				for (int i = 0; i < fileContents.Length; i++) {
+					CoverageType type = CoverageType.NotProfiled;
+
+					// Statement line numbers start at 1
+					if (file.Statements.ContainsKey(i + 1)) {
+						CoverageModel.CoverageStatement statement = file.Statements[i + 1];
+
+						if (statement.Counter == 0) {
+							type = CoverageType.NotCovered;
+						} else {
+							type = CoverageType.Covered;
+						}
+					}
+
+					AddLineToBuffer (fileContents[i], i, type);
+				}
+			}
+		};
+	}
+
+	protected void OnOpen (object sender, EventArgs e)
+	{
+		FileChooserDialog dialog = new FileChooserDialog ("Select coverage file", this, FileChooserAction.Open, 
+			"Cancel", ResponseType.Cancel, "Open", ResponseType.Accept);
+
+		if (dialog.Run () == (int) ResponseType.Accept) {
+			CoverageModel model = new CoverageModel (dialog.Filename);
+			Model = model;
+		}
+
+		dialog.Destroy ();
+	}
+
+	protected void OnQuit (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+}
