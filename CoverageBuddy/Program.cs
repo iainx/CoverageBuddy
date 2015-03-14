9d0ad111e8a41dfa46a0e67ce77411299b4e8cf5
Iain <iain@sleepfive.com>
2015-03-14 20:20:11 +0000

    An initial commit

---
+++ CoverageBuddy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+namespace CoverageBuddy
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+
+			//CoverageModel model = new CoverageModel ("/Users/iain/Projects/xamarin/hello/test.xml");
+			MainWindow win = new MainWindow ();
+
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
